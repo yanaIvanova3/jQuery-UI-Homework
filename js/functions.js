65721906cb2d49acc83beec5ca65d0168f1cf736
Yana Ivanova <y.m.ivanova23@gmail.com>
2016-02-08 11:40:13 +0200

    Validations & modal > fixed

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,8 @@
  */
 var errors = [];
 
+
+//CHECK IF THERE ARE NO EMPTY IMPUTS
 function notEmptyInput(param) {
 	var paramVal = param.val();
 	if (!paramVal){
@@ -14,6 +16,8 @@ function notEmptyInput(param) {
 	}
 };
 
+
+//CHECK IF INPUTS ONLY CONTAIN NUMBERS
 function onlyNumbers(param) {
 	var paramVal = param.val();
 	if (!(paramVal.match(/^\d+$/))) {
@@ -25,6 +29,8 @@ function onlyNumbers(param) {
 	}
 };
 
+
+//CHECK IF INPUTS ONLY CONTAIN LETTERS
 function onlyLetters(param) {
 	var paramVal = param.val();
 	if (!(paramVal.match(/^[a-zA-Z]*$/))) {
@@ -36,14 +42,55 @@ function onlyLetters(param) {
 	}
 };
 
+
+//CHECK DATES FROM DATEPICKERS
 function checkDays() {
+	var todaysDate = new Date();
 	var fromDate = new Date($('#fromDate').datepicker('getDate'));
 	var toDate = new Date($('#toDate').datepicker('getDate'));
 	var msDiff = toDate - fromDate;
 	var daysDiff = msDiff / 1000 / 60 / 60 / 24;
 	
+	if(fromDate < todaysDate || toDate < todaysDate || toDate < fromDate) {
+		errors.push('You have picked invalid dates!');
+		$('.datepicker').css('border', '1px groove red');
+		return false;
+	}
+	
 	if (daysDiff != 3) {
 		errors.push('You should select exactly 3 days for your vacation');
 		$('.datepicker').css('border', '1px groove red');
+		return false;
+	} else {
+		return true;
 	}
 }
+
+
+//CHECK GUESTS COUNT
+function checkGuestsCount(param) {
+	var paramVal = param.val();
+	
+	if(paramVal <= 0) {
+		errors.push('You should enter valid guests count (at least 1)');
+		param.css('border', '1px groove red');
+		return false;
+	}
+		return true;
+	
+}
+
+
+
+//PICKED DESTINATIONS COUNT
+function checkDestinationsCount(counter) {
+		if(counter != 3) {
+			errors.push('You should drag 3 destinations!');
+			return false;
+		} else {
+			return true;
+		}
+}
+
+
+
--- js/main.js
+++ js/main.js
@@ -3,9 +3,74 @@
  */
 
 $(document).ready(function() {
-	$('#submit').on('click', function() {	
+	var fromDate = $('#fromDate');
+	var toDate = $('#toDate');
+	var firstName = $('#firstName');
+	var lastName = $('#lastName');
+	var email = $('#email');
+	var country = $('#country');
+	var address = $('#address');
+	var phone = $('#phone');
+	var spinner = $('#spinner');
+	var counter = 0;
+	var chosenDestinations = $(".dropped");
+	
+	//jQUERY UI ELEMENTS 'ACTIVATION'
+	$( "#fromDate" ).datepicker();
+	$( "#toDate" ).datepicker();
+	$( ".draggable" ).draggable({ 
+		revert: "invalid",
+		drop: function (event, ui) {
+			$(event.target).addClass('dropped');
+		}
+	});
 
+	$("#droppable").droppable({
+		drop: function( event, ui ) {
+			counter++;
+			console.log(counter);
+		}
+	});
+	$( "#spinner" ).spinner();
+	$('button').button();
+
+
+	$('#submit').on('click', function() {	
+		notEmptyInput(fromDate);
+		notEmptyInput(toDate);
+		notEmptyInput(firstName);
+		notEmptyInput(lastName);
+		notEmptyInput(email);
+		notEmptyInput(country);
+		notEmptyInput(address);
+		notEmptyInput(phone);
+		notEmptyInput(spinner);
 		
+		onlyNumbers(phone);
+		onlyLetters(firstName);
+		onlyLetters(lastName);
+		onlyLetters(country);
+		
+		checkDays();
+		
+		checkGuestsCount(spinner);
+		
+		checkDestinationsCount(counter);
+		
+		if(errors.length != 0) {
+			alert(errors.join('\n'));
+			errors = [];
+			return;
+		} else {
+			$( "#modal" ).dialog({
+				  modal: true,
+				  autoOpen: true,
+				  height: 400,
+				  width: 800,
+				  position: top
+			});
+			$("#modal-text").text("		Dear " + firstName.val() + " " + lastName.val() + "	, your reservation is complete! We are expecting you at 06:00AM at '13-th Square' N85 on the " + fromDate.val() + ". Please be accurate! If you have any questions, please contact us : (+349)-123-456-789! Have a good day!");
+		}
 		
 	});
 });
